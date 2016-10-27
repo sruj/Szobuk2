9edb7b06c9be499c93b3fe3031cb8e861e56c4f0
sruj <chryplewiczpawel@wp.pl>
2016-10-28 00:02:26 +0200

    testy funkcjonalne. KlientControllerTest.

--- src/AppBundle/Tests/Controller/KlientControllerTest.php
+++ src/AppBundle/Tests/Controller/KlientControllerTest.php
@@ -8,15 +8,23 @@ class KlientControllerTest extends WebTestCase
 {
     public function testHistoriapanel()
     {
-        //refaktor: Type error: Argument 2 passed to Doctrine\ORM\EntityRepository::__construct() must be an instance of Doctrine\ORM\Mapping\ClassMetadata, none given, called in C:\wamp64\www\Szobuk2\app\cache\dev\appDevDebugProjectContainer.php on line 455
-        //500 Internal Server Error - FatalThrowableError
         $client = static::createClient([], [
             'PHP_AUTH_USER' => 'chinczyk',
             'PHP_AUTH_PW' => 'hinolp9',
         ]);
 
-        $crawler = $client->request('GET', '/historiaPanel');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /kategoria/");
-    }
+        $crawler = $client->request('GET', '/profile/');
+        $crawler = $client->click($crawler->selectLink('Historia Zamówień')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /historiaPanel/");
 
+        $this->assertGreaterThan(4, $crawler->filterXPath('//thead/tr/th')->count(), 'Missing elements //thead/tr/th - means there is no table head ' );
+        $this->assertGreaterThan(10, $crawler->filterXPath('//tbody/tr/td[2]')->count(), 'Missing elements //tbody/tr/td[2] - means there is no at last 10 rows in table');
+
+        $crawler = $client->click($crawler->filterXPath('//tbody/tr[1]/td[6]/a')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /historiaPanel/");
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Dane do wysyłki:")')->count(),
+            'strona /panel/szczegoly-zamowienia/1/2/ nie ma tekstu "Dane do wysyłki:" ');
+
+    }
 }
