74d9a00641c4a9d7197a667493fb034fcca58a1a
sruj <chryplewiczpawel@wp.pl>
2016-12-15 15:26:01 +0100

    PHPUnit. Testy jednostkowe. Filter.php. Działa pięknie. Kupa roboty bo zmockowałem 3 obiekty które wykorzystywane są w tej klasie. Nie wiem czy to było niezbędne by dublować każdą użytą metodę, czy np nie wystarczy by wszystkie pośrednie metody zwracały null, przyjmowały puste mocki a by jedynie ostatnia pośrednia metoda zwracała to co należy, bo przecież i tak na końcu robię assert czy wynikiem jest to co zwraca ta ostatnia pośrednia metoda. No chodzi o to że testowana etoda publiczna prepareStatusFilterQuery() wyłuje pośrednio z 6 metod prywatnych i z 20 metod obiektów zależnych a ostatnim krokiem jest wywołanie metody której wynik i tak ja dubluję sztucznie (->will($this->returnValue($tableDetailsObject)) a chwilę później sprawdzam assertem czy testowana publiczna metoda zwraca obiekt tego typu (assertInstanceOf)

--- src/AppBundle/Tests/Utils/Manager/FilterTest.php
+++ src/AppBundle/Tests/Utils/Manager/FilterTest.php
@@ -10,44 +10,114 @@ namespace AppBundle\Tests\Utils\Manager;
 
 use AppBundle\Utils\Manager\Filter;
 use AppBundle\Utils\Manager\TableDetails;
-use AppBundle\Form\Filter\StatusType;
-use AppBundle\Form\Filter\DataZamType;
-use AppBundle\Form\Filter\NrKlientaType;
+use AppBundle\Utils\Manager\FilterQuery;
+use AppBundle\Utils\Manager\FormsManagerExtended;
 
 class FilterTest extends \PHPUnit_Framework_TestCase
 {
 
-
-    public function testStub()
+    public function testPrepareFilterAndQuery()
     {
-
-        $td = $this->createMock(TableDetails::class);
-        $td->method('getColumnsSortOrder')->willReturn('');
-        $td->method('getColumnSort')->willReturn('');
-        $td->method('getFilterField')->willReturn('');
-        $td->method('getQuery')->willReturn('');
-        $td->method('getFilter')->willReturn('');
-        $td->method('getIdentifier')->willReturn('');
-
-        $forms['StatusForm'] = $this->createMock(StatusType::class);
-        $forms['StatusForm']->method('isValid')->willReturn(true);
-        $forms['StatusForm']->method('getIdstatus')->willReturn(true);
-        $forms['DataZamForm'] = $this->createMock(DataZamType::class);
-        $forms['DataZamForm']->method('isValid')->willReturn(false);
-        $forms['NrKlientaForm'] = $this->createMock(NrKlientaType::class);
-        $forms['NrKlientaForm']->method('isValid')->willReturn(false);
-
-        $fq;
-
-
-
-
-        $filter = new Filter();
-
-        $this->assertEquals('foo', $filter->prepareFilterAndQuery($td,$forms,$fq));
+        $object = new Filter();
+        $res = $object->prepareFilterAndQuery(
+            $this->getTableDetails(),
+            $this->getFormsManagerExtended(),
+            $this->getFilterQueryMock());
+        $this->assertInstanceOf(TableDetails::class,$res);
     }
-    
-    
 
+    protected function getTableDetails(
+        $filter1 = false,
+        $filter2 = 'idstatus',
+        $identifier = false,
+        $query = false,
+        $filterField = false,
+        $columnSort = null,
+        $columnsSortOrder = null)
+    {
+        $TableDetails = $this->getMockBuilder(TableDetails::class)
+            ->getMock();
+        $TableDetails->expects($this->any())
+            ->method('getFilter')
+            ->will($this->onConsecutiveCalls($filter1,$filter2,$filter2));
+        $TableDetails->expects($this->any())
+            ->method('getQuery')
+            ->will($this->returnValue($query));
+        $TableDetails->expects($this->any())
+            ->method('getIdentifier')
+            ->will($this->returnValue($identifier));
+        $TableDetails->expects($this->any())
+            ->method('getColumnsSortOrder')
+            ->will($this->returnValue($columnsSortOrder));
+        $TableDetails->expects($this->any())
+            ->method('getColumnSort')
+            ->will($this->returnValue($columnSort));
+        $TableDetails->expects($this->any())
+            ->method('getFilterField')
+            ->will($this->returnValue($filterField));
+        
+        return $TableDetails;
+    }
 
+    
+    protected function getFormsManagerExtended(
+        $isAnyValid = true,
+        $isStatusFormValid = true,
+        $isDataZamFormValid = false,
+        $isNrKlientaFormValid = false,
+        $idStatus = 3,
+        $idKlient = 2,
+        $od = '2015-12-25 20:12:01',
+        $do = '2016-12-25 20:12:01')
+    {
+        $FormsManagerExtended = $this->getMockBuilder(FormsManagerExtended::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $FormsManagerExtended->expects($this->any())
+            ->method('isAnyFormValid')
+            ->will($this->returnValue($isAnyValid));
+        $FormsManagerExtended->expects($this->any())
+            ->method('isStatusFormValid')
+            ->will($this->returnValue($isStatusFormValid));
+        $FormsManagerExtended->expects($this->any())
+            ->method('isDataZamFormValid')
+            ->will($this->returnValue($isDataZamFormValid));
+        $FormsManagerExtended->expects($this->any())
+            ->method('isNrKlientaFormValid')
+            ->will($this->returnValue($isNrKlientaFormValid));
+        $FormsManagerExtended->expects($this->any())
+            ->method('getIdStatusFromStatusForm')
+            ->will($this->returnValue($idStatus));
+        $FormsManagerExtended->expects($this->any())
+            ->method('getOdFromDataZamForm')
+            ->will($this->returnValue($od));
+        $FormsManagerExtended->expects($this->any())
+            ->method('getDoFromDataZamForm')
+            ->will($this->returnValue($do));
+        $FormsManagerExtended->expects($this->any())
+            ->method('getIdKlientFromNrKlientaForm')
+            ->will($this->returnValue($idKlient));
+
+        return $FormsManagerExtended;
+    }
+
+    
+    protected function getFilterQueryMock()
+    {
+        $td = new TableDetails();
+        $FilterQuery = $this->getMockBuilder(FilterQuery::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $FilterQuery->expects($this->any())
+            ->method('prepareStatusFilterQuery')
+            ->will($this->returnValue($td));
+        $FilterQuery->expects($this->any())
+            ->method('prepareDataFilterQuery')
+            ->will($this->returnValue($td));
+        $FilterQuery->expects($this->any())
+            ->method('prepareKlientFilterQuery')
+            ->will($this->returnValue($td));
+        
+        return $FilterQuery;
+    }
 }
