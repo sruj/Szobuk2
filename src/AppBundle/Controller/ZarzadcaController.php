c6b37d42d6dc31f61044a01419c8908d9d004cef
sruj <chryplewiczpawel@wp.pl>
2016-12-18 20:16:42 +0100

    refaktoryzacja. drobnica

--- src/AppBundle/Controller/ZarzadcaController.php
+++ src/AppBundle/Controller/ZarzadcaController.php
@@ -39,6 +39,10 @@ class ZarzadcaController extends Controller
     }
 
     /**
+     * TODO: zdaje się że był zamysł by móc filtrować już przefiltrowane dane, ale zmienne filter i filterField służą tylko do zmiany sortowania przefiltrowanych wcześniej danych.
+     * 
+     * TODO: z jakiegoś powodu, przestał zapamiętywac poprzednie filtrowanie gdy sortuję. I próba innego sortowania przefiltrowanej tabeli wyswietla wyniki dla wszystkich danych a nie poprzednio filtrowanych.  
+     * 
      * @Route("/panel/{filter}-{identifier}", name="panelSortFromDetails",
      *      defaults={
      *     "filter": false,
