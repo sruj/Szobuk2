75cf9b437bef4ea6311f824f861197283a26fd04
sruj <chryplewiczpawel@wp.pl>
2017-02-27 15:47:56 +0100

    TrenningBundle. Challenge 4. Ajax+Symfony. Bez oglądania rozdziału 4 zrobiłem po swojemu to co ma być efektem końcowym: Zmiana atrybutu klasy znacznika przy naciśnięciu guzika ORAZ zachowanie zmian po odświeżeniu strony z użyciem sesji. Czyli przyn naciśnięciu guzika "Favourite" dla jednego z paragrafów, ten paragram zmienia styl, guzik zmienia nazwę na "Unfavourite". (działa)

--- src/TrenningBundle/Controller/DefaultController.php
+++ src/TrenningBundle/Controller/DefaultController.php
@@ -4,14 +4,56 @@ namespace TrenningBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/t")
+     * Chapter 3
+     * @Route("/3")
      */
     public function indexAction()
     {
-        return $this->render('TrenningBundle:Default:index.html.twig');
+        return $this->render('TrenningBundle:Default:chapter3.html.twig');
+    }
+
+    /**
+     * Chapter 4
+     * @Route("/4")
+     */
+    public function chap4Action(Request $request)
+    {
+        $session = $request->getSession();
+        
+        
+        if (!$session->get('fav')){
+            $fav=[];
+            $fav['p101']='Favorite';
+            $fav['p102']='Favorite';
+            $fav['p103']='Favorite';
+            $session->set('fav',$fav);
+        }
+        
+        return $this->render('TrenningBundle:Default:chapter4.html.twig');
+    }
+
+
+    /**
+     * Chapter 4
+     * @Route(name="c4", options={"expose"=true})
+     */
+    public function chap4ReceiveAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        $f = $request->get('data');
+        foreach ($f as $k => $v){
+            $fav[substr($k, -4)]=$v;
+        }
+        $replaced = array_replace($session->get('fav'),$fav);
+
+        $session->set('fav',$replaced );
+
+        return [];
     }
 }
